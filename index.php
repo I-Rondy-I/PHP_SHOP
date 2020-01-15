1c69cac8e56220c80e3969881aa698213d1e8990
Aleksei Prykhodko <206216@edu.p.lodz.pl>
2020-01-15 01:06:01 +0100

    Update_3

--- index.php
+++ index.php
@@ -16,3 +16,90 @@
 	<a href = "template-login/signIn.php">Sign In</a><br>
 	<a href = "template-register/signUp.php">Sign Up</a>
 <?php endif; ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/icon.jpg" />
+        <title>Sklep Gier</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+           <?php
+            require 'header.php';
+           ?>
+           <div id="bannerImage">
+               <div class="container">
+                   <center>
+                   <div id="bannerContent">
+                       <h1>Sklep Gier</h1>
+                       <a href="template-shop/shop.php" class="btn btn-danger">Do zakupów</a>
+                   </div>
+                   </center>
+               </div>
+           </div>
+           <div class="container">
+               <div class="row">
+                   <div class="col-xs-4">
+                       <div  class="thumbnail">
+                           <a href="template-shop/shop.php">
+                                <img src="img/pc.jpg" alt="Camera">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                        <p id="autoResize">PC</p>
+                                        <p>Personal Computer</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="template-shop/shop.php">
+                               <img src="img/ps4.jpg" alt="Watch">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                    <p id="autoResize">PS4</p>
+                                    <p>Playstation 4</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="template-shop/shop.php">
+                               <img src="img/xone.jpg" alt="Shirt">
+                           </a>
+                           <center>
+                               <div class="caption">
+                                   <p id="autoResize">XOne</p>
+                                   <p>Xbox One</p>
+                               </div>
+                           </center>
+                       </div>
+                   </div>
+               </div>
+           </div>
+            <br><br> <br><br><br><br>
+           <footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>Tu może być - Reklama;)</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
--- template-shop/shop.php
+++ template-shop/shop.php
@@ -39,9 +39,12 @@ if(empty($_SESSION["shopping_cart"])) {
 <html>
 <head>
 <title>Sklep Gier</title>
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/slick.css"/>
 <link rel='stylesheet' href='css1/style.css' type='text/css' media='all' />
 </head>
 <body>
+
 <div style="width:700px; margin:50 auto;">
 
 <h2>Sklep Gier</h2>   
@@ -50,26 +53,32 @@ if(empty($_SESSION["shopping_cart"])) {
 if(!empty($_SESSION["shopping_cart"])) {
 $cart_count = count(array_keys($_SESSION["shopping_cart"]));
 ?>
-<div class="cart_div">
-<a href="cart.php"><img src="cart-icon.png" /> Cart<span><?php echo $cart_count; ?></span></a>
-</div>
+
+<div class="carousel clearfix">
+<h2>Simple and Responsive jQuery Carousel Slider</h2>
+
+<div class="carousel-view clearfix">
+
 <?php
 }
 
 $result = mysqli_query($con,"SELECT * FROM `gra`");
 while($row = mysqli_fetch_assoc($result)){
-		echo "<div class='product_wrapper'>
-			  <form method='post' action=''>
-			  <input type='hidden' name='id' value=".$row['id']." />
-			  <div class='image'><img src='".$row['image']."' /></div>
-			  <div class='tytul'>".$row['tytul']."</div>
-		   	  <div class='cena'>$".$row['cena']."</div>
-			  <button type='submit' class='buy'>Buy Now</button>
-			  </form>
+		echo "<div class='box'>
+				  <form method='post' action=''>
+					  <input type='hidden' name='id' value=".$row['id']." />
+					  <img src='".$row['image']."' style='width:100%' />
+					  <p>".$row['tytul']."</p>
+					  <p class='price'>$".$row['cena']."</p>
+					  <button type='submit' class='buy'>Buy Now</button>
+				  </form>
 		   	  </div>";
         }
 mysqli_close($con);
 ?>
+
+</div> <!-- carosel-view End -->
+
 
 <div style="clear:both;"></div>
 
@@ -79,5 +88,34 @@ mysqli_close($con);
 
 <br /><br />
 </div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script>
+$('.carousel-view').slick({
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+</script>	
+
 </body>
 </html>
