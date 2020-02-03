bbfd705c4307bba54a14ba4ff258b9b3a797877c
Aleksei Prykhodko <206216@edu.p.lodz.pl>
2020-02-03 18:34:29 +0100

    Update_8

--- template-shop/cart.php
+++ template-shop/cart.php
@@ -16,19 +16,20 @@ if(!empty($_SESSION["shopping_cart"])) {
 }
 
 if (isset($_POST['action']) && $_POST['action']=="change"){
-  foreach($_SESSION["shopping_cart"] as &$value){
-    if($value['id'] === $_POST["id"]){
-        $value['quantity'] = $_POST["quantity"];
-        break; // Stop the loop after we've found the product
-    }
-}
-  	
+    foreach($_SESSION["shopping_cart"] as &$value){
+		if($value['id'] === $_POST["id"]){
+			$value['quantity'] = $_POST["quantity"];
+			break; // Stop the loop after we've found the product
+		}
+    }	
 }
 ?>
 <html>
 	<head>
 		<title>Koszyk</title>
-		
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				
 			<!-- Icons font header-->
 			<link rel="shortcut icon" href="../img/icon.jpg" />
 			<!-- latest compiled and minified CSS -->
@@ -39,29 +40,19 @@ if (isset($_POST['action']) && $_POST['action']=="change"){
 			<script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
 			<!-- External CSS -->
 			<link rel="stylesheet" href="../css/style.css" type="text/css">
+
+				<link rel="stylesheet" href="cart/style.css" media="screen" title="no title" charset="utf-8">
+				<script src="https://code.jquery.com/jquery-2.2.4.js" charset="utf-8"></script>
+				<meta name="robots" content="noindex,follow" />
 		
 		<link rel='stylesheet' href='css1/style.css' type='text/css' media='all' />
 	</head>
 	<body>
 		<?php
         require '../header-template.php';
-		?>
+		?>	
+		<h2 style="margin-top:8; text-align: center;"><strong style="text-align: center; background-color:yellow; font-size: 30;">Koszyk</strong></h2>  
 		<div style="width:700px; margin:50 auto;">
-			<h2>Koszyk</h2>   
-			<?php
-			if(!empty($_SESSION["shopping_cart"])) {
-			$cart_count = count(array_keys($_SESSION["shopping_cart"]));
-			?>
-			<div class="cart_div">
-				<a href="cart.php">
-					<img src="cart-icon.png" /> Cart
-					<span><?php echo $cart_count; ?></span>
-				</a>
-			</div>
-			<?php
-			}
-			?>
-
 			<div class="cart">
 				<?php
 				if(isset($_SESSION["shopping_cart"])){
@@ -69,56 +60,61 @@ if (isset($_POST['action']) && $_POST['action']=="change"){
 				?>	
 				<table class="table">
 					<tbody>
-						<tr>
-							<td></td>
-							<td>Tytul</td>
-							<td>Ilosc</td>
-							<td>Cena</td>
-							<td>Produkt Razem</td>
+						<tr bgcolor="purple">
+							<td bgcolor="orange"></td>
+							<td bgcolor="orange"></td>
+							<td bgcolor="orange">Tytul</td>
+							<td bgcolor="orange">Ilosc</td>
+							<td bgcolor="orange">Cena</td>
+							<td bgcolor="orange">Razem</td>
 						</tr>	
 						<?php		
 						foreach ($_SESSION["shopping_cart"] as $product){
 						?>
-						<tr>
-							<td><img src='<?php echo $product["image"]; ?>' width="50" height="40" /></td>
-							<td><?php echo $product["tytul"]; ?><br />
-								<form method='post' action=''>
-									<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
-									<input type='hidden' name='action' value="remove" />
-									<button type='submit' class='remove'>Usun</button>
-								</form>
-							</td>
-							<td>
-								<form method='post' action=''>
-									<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
-									<input type='hidden' name='action' value="change" />
-									<select name='quantity' class='quantity' onchange="this.form.submit()">
-										<option <?php if($product["quantity"]==1) echo "selected";?> value="1">1</option>
-										<option <?php if($product["quantity"]==2) echo "selected";?> value="2">2</option>
-										<option <?php if($product["quantity"]==3) echo "selected";?> value="3">3</option>
-										<option <?php if($product["quantity"]==4) echo "selected";?> value="4">4</option>
-										<option <?php if($product["quantity"]==5) echo "selected";?> value="5">5</option>
-									</select>
-								</form>
-							</td>
-							<td><?php echo "$".$product["cena"]; ?></td>
-							<td><?php echo "$".$product["cena"]*$product["quantity"]; ?></td>
-						</tr>
+						<div class="shopping_cart">
+							<tr bgcolor="white">
+								<td style="text-align: center;"><div class="buttons"><span class="like-btn"></span></div></td>
+							
+								<td><img src='<?php echo $product["image"]; ?>' width="80" height="110" /></td>
+								<td style="vertical-align: middle;"><?php echo $product["tytul"]; ?><br />
+									<form method='post' action=''>
+										<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
+										<input type='hidden' name='action' value="remove" />
+										<button type='submit' class='remove'>Usun</button>
+									</form>
+								</td>
+								<td style="vertical-align: middle;">
+									<form method='post' action=''>
+										<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
+										<input type='hidden' name='action' value="change" />
+										<select name='quantity' class='quantity' onchange="this.form.submit()">
+											<option <?php if($product["quantity"]==1) echo "selected";?> value="1">1</option>
+											<option <?php if($product["quantity"]==2) echo "selected";?> value="2">2</option>
+											<option <?php if($product["quantity"]==3) echo "selected";?> value="3">3</option>
+											<option <?php if($product["quantity"]==4) echo "selected";?> value="4">4</option>
+											<option <?php if($product["quantity"]==5) echo "selected";?> value="5">5</option>
+										</select>
+									</form>
+								</td>
+								<td style="vertical-align: middle;"><?php echo "$".$product["cena"]; ?></td>
+								<td style="vertical-align: middle;"><?php echo "$".$product["cena"]*$product["quantity"]; ?></td>
+							</tr>
+						</div>
 						<?php
 						$total_price += ($product["cena"]*$product["quantity"]);
 						}
 						?>
 						<tr>
-							<td colspan="5" align="right">
-								<strong>Razem: <?php echo "$".$total_price; ?></strong>
+							<td colspan="5" align="right" style="text-align:50px;">
+								<strong style="background-color:yellow; font-size: 30">Do oplaty: <?php echo "$".$total_price; ?></strong>
 							</td>
 						</tr>
 					</tbody>
 				</table>		
 				  <?php
 				}else{
-					echo "<h3>Your cart is empty!</h3>";
-					}
+					echo '<h3 style = "color:red; margin-top:200; text-align:center; font-weight:666; text-size:150; font-size:60; background-color:yellow;">Your cart is empty!</h3>';
+				 }
 				?>
 			</div>
 			<div style="clear:both;"></div>
@@ -134,5 +130,41 @@ if (isset($_POST['action']) && $_POST['action']=="change"){
                </center>
                </div>
         </footer>
+		<script type="text/javascript">
+		  $('.minus-btn').on('click', function(e) {
+				e.preventDefault();
+				var $this = $(this);
+				var $input = $this.closest('div').find('input');
+				var value = parseInt($input.val());
+
+				if (value > 1) {
+					value = value - 1;
+				} else {
+					value = 0;
+				}
+
+			$input.val(value);
+
+			});
+
+			$('.plus-btn').on('click', function(e) {
+				e.preventDefault();
+				var $this = $(this);
+				var $input = $this.closest('div').find('input');
+				var value = parseInt($input.val());
+
+				if (value < 100) {
+				value = value + 1;
+				} else {
+					value =100;
+				}
+
+				$input.val(value);
+			});
+
+		  $('.like-btn').on('click', function() {
+			$(this).toggleClass('is-active');
+		  });
+		</script>
 	</body>
 </html>
--- template-shop/delicious-shopping-cart/README.markdown
+++
@@ -1,4 +0,0 @@
-# Delicious Shopping Cart
- _A Pen created at CodePen.io. Original URL: [https://codepen.io/I-Rondy-I/pen/OJPBBNe](https://codepen.io/I-Rondy-I/pen/OJPBBNe).
-
- Animated shopping cart with a "gourmandism" theme, with a ui inspired by touchscreen interfaces. The  cart list is displayed horizontally. Hover the list to make it scroll and alter quantities.
